7bfdca01823d4d0f79d5b0189f28eb11ba076df8
Alex Brinister <brinisal@tutamail.com>
2021-04-26 08:28:22 -0400

    Added mandelbrot exercise

--- rusted-calculator/src/main.rs
+++ rusted-calculator/src/main.rs
@@ -1,3 +1,14 @@
 fn main() {
-    println!("Welcome to Rusted Calculator, a dumb Reverse Polish Notation calculator");
+    println!("Welcome to Rusted Calculator, a Reverse Polish Notation calculator");
+
+    // 0. Parse the command-line arguments
+    
+    // 1. Parse out the command and arguments
+    
+    // 2. Run the parsed command with given arguments
+    
+    // 3. Return the result
 }
+
+/// The add function; takes any two numerical types and adds them together
+fn add() {}
